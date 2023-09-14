0c1c501699e93e1c6c3fd035fba3b9e9fbee7322
Leandro Barbosa <leandroh_99@outlook.com>
2023-09-13 22:04:30 -0300

    more

---
+++ coke/coke.py
@@ -0,0 +1,25 @@
+def main():
+    put_coins()
+
+def put_coins():
+
+    # check what coin was inserted and the amount dued
+    coke_price = 50
+    while True:
+        coins = int(input("Insert a coin: "))
+        match coins:
+            case 25:
+                coke_price -= 25
+            case 10:
+                coke_price -= 10
+            case 5:
+                coke_price -= 5
+            case _:
+                print("Insert a valid coin!")
+        if coke_price > 0:
+            print(f"Amount Due: {coke_price}")
+        elif coke_price <= 0:
+            print(f"Change Owed: {coke_price * - 1}")
+            break
+
+main()
---
+++ twttr/twttr.py
@@ -0,0 +1,19 @@
+def main():
+    # call the function that is gonna erase the vowels
+    phrase = str(input("Type any phrase: "))
+    vowel_out(phrase)
+
+def vowel_out(p):
+    vowels = ["a", "e", "i", "o", "u"] # creates a list of the vowels
+
+    # check and ignore any vowels
+    new_word = []
+    for i in range(len(p)):
+        if p[i].lower() not in vowels:
+            new_word.append(p[i])
+
+    # add the letter besides any vowel to a new variable and print it
+    new_word = "".join(new_word)
+    print(new_word)
+
+main()
